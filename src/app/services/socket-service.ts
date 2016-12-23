7bed2035de616630b8ed320f8d6390045473a211
AnoopDhillon <anoopdhillon30@gmail.com>
2016-12-22 16:43:34 -0800

    renamed chat-service to socket-service

---
+++ src/app/services/socket-service.ts
@@ -0,0 +1,35 @@
+import { Message } from '../models/message';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+declare var io:any;
+
+export class SocketService {
+    private url = 'http://localhost:3000';
+    private socket;
+    private _messages: Subject<any> = new Subject();
+    public messages: Observable<any>;
+    
+    constructor(){
+        this.messages = this._messages.asObservable();        
+        this.socket = io(this.url);
+        this.socket.on('chat message', (username, msg) => {
+            this._messages.next({username,msg})
+        });
+    }
+
+    sendMessage(msg): boolean {
+        if (this.socket !== null) {
+            this.socket.emit('chat message', msg);
+            return true;
+        }
+        return false;
+    }
+
+    sendLogin(nickname): boolean {
+        if (this.socket !== null) {
+            this.socket.emit('login', nickname);
+            return true;
+        }
+        return false;
+    }
+}
