0a6db8d409ae0f80c3f47ac4f636af3e2aa0d094
unknown <anoopdhillon30@gmail.com>
2017-09-04 18:23:20 -0700

    Light refactor, pushing .gitignore

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,14 +15,14 @@ let server = http.createServer(app);
 let io = socketIO(server);
 
 let userList = [
-    {
-        'username': 'Anoop',
-        'password': 'aa'
-    },
-    {
-        'username': 'Me',
-        'password': 'me'
-    }
+	{
+		'username': 'Anoop',
+		'password': 'aa'
+	},
+	{
+		'username': 'Me',
+		'password': 'me'
+	}
 ];
 
 let activeUsers = [];
@@ -34,85 +34,85 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Middleware
 apiRouter.use((request, response, next) => {
-    let token = request.body.token || request.query.token || request.headers['x-access-token'];
-    if (token) {
-        jwt.verify(token, 'secret', (err, decoded) => {
-            if (err) {
-                response.status(403).send({ status: '403' });
-            } else {
-                if (findName(request.body.username, request.body.password)) {
-                    next();
-                } else {
-                    response.status(401).json({ status: '401' });
-                    response.end();
-                    return;
-                }
-            }
-        });
-    } else {
-        return response.status(403).send({ status: '403' });
-    }
+	let token = request.body.token || request.query.token || request.headers['x-access-token'];
+	if (token) {
+		jwt.verify(token, 'secret', (err, decoded) => {
+			if (err) {
+				response.status(403).send({ status: '403' });
+			} else {
+				if (findName(request.body.username, request.body.password)) {
+					next();
+				} else {
+					response.status(401).json({ status: '401' });
+					response.end();
+					return;
+				}
+			}
+		});
+	} else {
+		return response.status(403).send({ status: '403' });
+	}
 });
 
 // static content
 app.get('/app|res|styles|scripts|server|lib',
-    (req: express.Request, res: express.Response) => {
-        res.sendFile(req.path, { root: 'dist' });
-    });
+	(req: express.Request, res: express.Response) => {
+		res.sendFile(req.path, { root: 'dist' });
+	});
 
 app.post('/login', (req: any, res: any) => {
-    if (findName(req.body.username, req.body.password)) {
-        let token = jwt.sign({ username: req.body.username }, 'secret', { expiresIn: 60 * 60 });
-        res.json({ 'status': 200, 'token': token, 'expiresAt': Date.now() + (60 * 60 * 1000) });
-    } else {
-        res.status(403).send({ status: '403' });
-    }
+	if (findName(req.body.username, req.body.password)) {
+		let token = jwt.sign({ username: req.body.username }, 'secret', { expiresIn: 60 * 60 });
+		res.json({ 'status': 200, 'token': token, 'expiresAt': Date.now() + (60 * 60 * 1000) });
+	} else {
+		res.status(403).send({ status: '403' });
+	}
 });
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../index.html'));
+	res.sendFile(path.join(__dirname, '/../index.html'));
 });
 
 io.on('connection', function (socket) {
-    let sid = socket.id;
-    let username;
+	let sid = socket.id;
+	let username;
 
-    messageHistory.forEach(function (post) {
-        socket.emit('chat message', post.name, post.message);
-    });
+	messageHistory.forEach(function (post) {
+		socket.emit('chat message', post.name, post.message);
+	});
 
-    socket.on('chat message', function (msg) {
-        messageHistory.push({
-            name: username,
-            message: msg
-        });
-        io.emit('chat message', username, msg);
-    });
+	socket.on('chat message', function (msg) {
+		messageHistory.push({
+			name: username,
+			message: msg
+		});
+		io.emit('chat message', username, msg);
+	});
 
-    socket.on('disconnect', function () {
-        io.emit('alert', username + ' has disconnected');
-    });
+	socket.on('disconnect', function () {
+		io.emit('alert', username + ' has disconnected');
+	});
 
-    socket.on('login', function (data) {
-        username = data.username;
-        activeUsers.push({
-            id: sid,
-            name: data.username,
-            password: data.password
-        });
-        socket.broadcast.emit('alert', username + ' has connected');
-    });
+	socket.on('login', function (data) {
+		username = data.username;
+		activeUsers.push({
+			id: sid,
+			name: data.username,
+			password: data.password
+		});
+		socket.broadcast.emit('alert', username + ' has connected');
+	});
 });
 
 function findName(uname, pword) {
-    for (let entry of userList) {
-        if (uname === entry.username && pword === entry.password) {
-            return true;
-        }
-    }
-    return false;
+	for (let entry of userList) {
+		if (uname === entry.username && pword === entry.password) {
+			return true;
+		}
+	}
+	return false;
 }
 
 server.listen(3000, function () {
-    console.log('listening on *:3000');
+	console.log('listening on *:3000');
 }); 
